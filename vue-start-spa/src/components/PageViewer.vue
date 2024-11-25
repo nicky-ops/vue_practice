da48db431fffca103627e5b1b79c993a7b49a841
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-11-25 22:16:11 +0300

    using router params

--- vue-start-spa/src/components/PageViewer.vue
+++ vue-start-spa/src/components/PageViewer.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="container">
         <h1 class="emphasize">Page Title</h1>
-        <p>content</p>
+        <p>{{$route.params.index}}</p>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['page'],
+   created() {
+        // console.log(this.$route.params);
+   } 
 }
 
 </script>
