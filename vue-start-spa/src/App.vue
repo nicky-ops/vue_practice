4777b583cc04dbd92a6502983581adf10dbb1561
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-11-25 21:48:27 +0300

    vue routes

--- vue-start-spa/src/App.vue
+++ vue-start-spa/src/App.vue
@@ -1,28 +1,30 @@
+/* eslint-disable */
 <template>
     <nav-bar 
         :pages="pages"
-        :activePage="activePage"
     ></nav-bar>
-    <page-viewer
+
+    <router-view></router-view>
+    <!-- <page-viewer
         v-if="pages.length > 0" 
         :page="pages[activePage]"
     ></page-viewer>
     <create-page
         @page-created="pageCreated"
-    ></create-page>
+    ></create-page> -->
 </template>
 
 <script>
 // import PageViewer from './components/PageViewer.vue';
 import NavBar from './components/NavBar.vue';
-import CreatePage from './components/CreatePage.vue';
+// import CreatePage from './components/CreatePage.vue';
 
 
 export default {
     components: {
         NavBar,
         // PageViewer
-        CreatePage
+        // CreatePage
     },
     created() {
         this.getPages();
--- vue-start-spa/src/main.js
+++ vue-start-spa/src/main.js
@@ -2,8 +2,11 @@ import {createApp} from 'vue';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import $bus from './utils/Events';
+import router from './routes';
 
 const app = createApp(App);
+
+app.use(router);
 
 app.config.globalProperties.$bus = $bus;
 
