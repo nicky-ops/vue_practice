52ddec02502cae41930b5a6b644d183b48529f1d
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-11-26 16:53:51 +0300

    using composition API to write components

--- vue-start-spa/src/views/PagesList.vue
+++ vue-start-spa/src/views/PagesList.vue
@@ -1,3 +1,34 @@
 <template>
-    <p>This is the pages list</p>
+    <div>
+        <p>{{data.counter}}</p>
+        <button @click.prevent="click">click</button>
+    </div>
 </template>
+
+<script setup>
+/* eslint-disable */
+import {ref, reactive} from 'vue';
+
+    // const counter = ref(0);
+
+    const data = reactive({
+        counter: 0
+    });
+
+    function click() {
+        // counter.value++;
+        data.counter++;
+    }
+    // export default {
+    //     data() {
+    //         return {
+    //             counter: 0
+    //         };
+    //     },
+    //     methods: {
+    //         click() {
+    //             this.counter++;
+    //         }
+    //     }
+    // }
+</script>
